ff28ff09ea24b9afa5135b7b9f0d715cc0f0a708
Koga Ryu <nelsonlzweni@gmail.com>
2022-08-13 00:06:10 +0200

    New: Animal Class, Dog Class & Birth Class

---
+++ src/Bird.java
@@ -0,0 +1,18 @@
+public class Bird extends Animal {
+    public Bird( String name ) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Breath in, breath out, repeat");
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Yorkie");
+        dog.breathe();
+        dog.eat();
+
+        Bird bird = new Bird("Parrot");
+        bird.breathe();
+        bird.eat();
+    }
+}
